5269b8d84f8a4a8bb248732a1b89f1a0772684e7
yueymiqua <pierre1116@gmail.com>
2021-07-29 17:00:28 -0400

    Update incidents.js

--- pages/incidents.js
+++ pages/incidents.js
@@ -1,75 +1,71 @@
 import { useState } from 'react'
-import { Input, InputLabel, FormHelperText, FormControl, Select, MenuItem, Button } from '@material-ui/core'
 import Link from 'next/link'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
 
 const incidents = () => {
-    const reports = [
-        {
-            subject: 'subject1',
-            description: 'description1',
-            username: 'user1',
-            department: 'HR'
-        },
-        {
-            subject: 'subject2',
-            description: 'description2',
-            username: 'user2',
-            department: 'Admin'
-        },
-        {
-            subject: 'subject3',
-            description: 'description3',
-            username: 'user3',
-            department: 'Finance'
-        }
-    ]
-    const [department, setDepartment] = useState('')
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('Username: ', username, 'Password: ', password, 'Department: ', department)
-        setUsername('');
-        setPassword('');
-        setDepartment('');
+    // const reports = [
+    //     {
+    //         subject: 'subject1',
+    //         description: 'description1',
+    //         username: 'user1',
+    //         department: 'HR'
+    //     },
+    //     {
+    //         subject: 'subject2',
+    //         description: 'description2',
+    //         username: 'user2',
+    //         department: 'Admin'
+    //     },
+    //     {
+    //         subject: 'subject3',
+    //         description: 'description3',
+    //         username: 'user3',
+    //         department: 'Finance'
+    //     }
+    // ]
+    
+    const createData = (subject, description, username, department) => {
+        return { subject, description, username, department };
     }
 
+    const reports = [
+        createData('subject1', 'description1', 'username1', 'HR'),
+        createData('subject2', 'description2', 'username2', 'Admin'),
+        createData('subject3', 'description3', 'username3', 'Finance')
+    ]
+
+    const [filterByUser, setFilterByUser] = useState('')
+
     return (
-        <div style={{ display: "flex", height: "100vh", justifyContent: "center", background: "lightGray", alignItems: "center"}}>
-            <form className='form-container' style={{textAlign: "center"}} onSubmit={(e) =>  handleSubmit(e)}>
-                <h1>Create New User</h1>
-                <div>
-                    <FormControl required>
-                        <InputLabel id="username-label" style={{background: "white"}}>Username</InputLabel>
-                        <Input id="username" placeholder="username" value={username} onInput={(e) => setUsername(e.target.value)}/>
-                    </FormControl>
-                </div>
-                <div>
-                    <FormControl required>
-                        <InputLabel id="password-label" style={{background: "white"}}>Password</InputLabel>
-                        <Input id="password" placeholder="password" value={password} onInput={(e) => setPassword(e.target.value)}/>
-                    <FormHelperText id="my-helper-text">We'll never share your password.</FormHelperText>
-                </FormControl>
-                </div>
-                <div>
-                    <FormControl required>
-                        <InputLabel id="department-label" style={{background: "white"}}>Department</InputLabel>
-                        <Select 
-                            style={{width: '160px'}}
-                            labelId="departments-label"
-                            id="departments"
-                            value={department}
-                            onChange={(e) => setDepartment(e.target.value)}
-                        >
-                            {departments.map((dept, index) => 
-                                <MenuItem key={index} value={dept}>{dept}</MenuItem>
-                            )}
-                        </Select>
-                    </FormControl>
-                </div>
-                <Button type="submit">Submit</Button><br></br>
-                <Link href="/">Back</Link>
-            </form>
-        </div>
+        <TableContainer component={Paper}>
+            <Table className="table" aria-label="incident-table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Subject</TableCell>
+                        <TableCell align="right">Details of Incident</TableCell>
+                        <TableCell align="right">Created By</TableCell>
+                        <TableCell align="right">Related Department</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {reports.map((report, index) => (
+                        <TableRow key={index}>
+                            <TableCell component="th" scope="row">{report.subject}</TableCell>
+                            <tableCell align="right">{report.description}</tableCell>
+                            <tableCell align="right">{report.username}</tableCell>
+                            <tableCell align="right">{report.department}</tableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
 
